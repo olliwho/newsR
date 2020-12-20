2183e0235bbfdee0037895cf0c96fbc42744fda2
Oliver Reichmann <olli.reichmann@gmail.com>
2020-12-20 09:47:15 +0100

    changed gh-pages link for deploying

--- client/src/template/Footer.tsx
+++ client/src/template/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
   return (
     <div className="app-grid-footer">
       <span className="app-name">newsR {packageJson.version}</span>
-      <a href="https://github.com/Elektropepi/newsR/" className="no-link github-link">
+      <a href="https://github.com/olliwho/newsR" className="no-link github-link">
         <FontAwesomeIcon icon={['fab', 'github']} />&nbsp;&nbsp;Github
       </a>
     </div>
