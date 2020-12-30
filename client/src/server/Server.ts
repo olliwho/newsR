121d95ef3757a0dc82daa20110f0efd95eca2940
Oliver Reichmann <olli.reichmann@gmail.com>
2020-12-30 01:39:02 +0100

    change newsgroup prefix to local storage

--- client/src/server/Server.ts
+++ client/src/server/Server.ts
@@ -203,7 +203,9 @@ export class Server implements ServerInterface {
 
   public async groups(): Promise<Group[]> {
     // todo: remove 'tu-graz*' once https://gitlab.com/timrs2998/newsie/merge_requests/2 is merged
-    const newsgroups = await this.newsieClient.listNewsgroups(process.env.REACT_APP_NNTP_GROUP_PREFIX);
+    const prefix = localStorage.getItem("nntpGroupPrefix");
+    const pre = prefix ? prefix : undefined;
+    const newsgroups = await this.newsieClient.listNewsgroups(pre);
     return newsgroups.newsgroups.map((ng) => {
       const description = typeof ng.description === 'undefined' ? '' : ng.description;
       return new Group(ng.name, description, this.host, this.newsieClient);
