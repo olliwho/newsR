301cc197edadfc7a34aa28e4bfff8e0318b15ee1
michaelpollak <github@michaelpollak.org>
2021-01-19 15:36:30 +0100

    Flipped attachment to the back of the table.

--- client/src/group/GroupDetail.tsx
+++ client/src/group/GroupDetail.tsx
@@ -163,7 +163,7 @@ export class GroupDetail extends React.Component<RouteComponentProps<GroupRouteP
                                       || null}/>
                       }/>
                       <Route path={`${match.path}`}>
-                        <Table onPressSort={(sortColumn, ascending) => this.setState({sortColumn: sortColumn, ascending: ascending})} sortColumn={this.state.sortColumn} ascending={this.state.ascending} data={articleListData} columns={[new TableColumn("📎", 0, "hasattachment"), new TableColumn("Subject", 0, "subject"), new TableColumn("Author", 1, "author"), new TableColumn("Date", 2, "date"), new TableColumn("Size", 0, "size")]} urlColumn="url"/>
+                        <Table onPressSort={(sortColumn, ascending) => this.setState({sortColumn: sortColumn, ascending: ascending})} sortColumn={this.state.sortColumn} ascending={this.state.ascending} data={articleListData} columns={[new TableColumn("Subject", 0, "subject"), new TableColumn("Author", 1, "author"), new TableColumn("Date", 2, "date"), new TableColumn("Size", 0, "size"), new TableColumn("📎", 0, "hasattachment")]} urlColumn="url"/>
 
                       </Route>
                     </Switch>
@@ -186,7 +186,7 @@ export class GroupDetail extends React.Component<RouteComponentProps<GroupRouteP
                                                     }
                                                  }))}/>}
 
-                      content1={<Table onPressSort={(sortColumn, ascending) => this.setState({sortColumn: sortColumn, ascending: ascending})} sortColumn={this.state.sortColumn} ascending={this.state.ascending} data={articleListData} columns={[new TableColumn("📎", 0, "hasattachment"), new TableColumn("Subject", 0, "subject"), new TableColumn("Author", 1, "author"), new TableColumn("Date", 2, "date"), new TableColumn("Size", 0, "size")]} urlColumn="url"/>}
+                      content1={<Table onPressSort={(sortColumn, ascending) => this.setState({sortColumn: sortColumn, ascending: ascending})} sortColumn={this.state.sortColumn} ascending={this.state.ascending} data={articleListData} columns={[new TableColumn("Subject", 0, "subject"), new TableColumn("Author", 1, "author"), new TableColumn("Date", 2, "date"), new TableColumn("Size", 0, "size"), new TableColumn("📎", 0, "hasattachment")]} urlColumn="url"/>}
 
                       content2={
                         <Switch>
